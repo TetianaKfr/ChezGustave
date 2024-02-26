6698042501ca0599f1833adb2315c58e359ba936
Brainfeed-1996 <olivierrobertduboille@gmail.com>
2024-02-26 17:38:46 +0100

    J'ai modifié ma page de profil

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './index.css';
-import { Search }  from './pages/Search/Search';
-import { Navbar } from './components/Navbar/Navbar';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import { Search } from "./pages/Search/Search";
+import { Navbar } from "./components/Navbar/Navbar";
+import Profile from "./pages/Profile/Profile.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        { /* Routes should be declared here. */ }
-        <Route path='/' element={ <Navbar /> } />
+        {/* Routes should be declared here. */}
+        <Route path="/" element={<Navbar />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
