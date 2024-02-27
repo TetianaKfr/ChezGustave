cf426bf4b4a9f8402a179a7180f6aeb0390d9217
Louis Le Cam <louislecam25@gmail.com>
2024-02-27 14:02:27 +0100

    Remove example code

--- server/src/middlewares/ExampleMiddlewares.ts
+++
@@ -1,6 +0,0 @@
-import { NextFunction, Request, Response } from 'express';
-
-export function ExampleMiddleware(req: Request, res: Response, next: NextFunction) {
-    console.log('Example log');
-    next();
-}
