07310c384a3d56d1a5fcec42aff0689b7011a73c
Louis Le Cam <louislecam25@gmail.com>
2024-02-28 15:24:23 +0100

    Add the create equipment route

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
 import UsersRouter from "./routes/UserRoutes";
+import EquipmentsRouter from "./routes/EquipmentRoutes";
 
 const app = express();
 
 app.use(express.json());
 
 app.use(UsersRouter);
+app.use(EquipmentsRouter);
 
 export default app;
