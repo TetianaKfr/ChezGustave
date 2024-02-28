9ed2900a0a199e203c4a6048ba1eda5ff7a5e06f
TetianaKfr <kosorukova.tetiana.a@gmail.com>
2024-02-28 12:24:12 +0100

    chenges roure reclamations

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -19,11 +19,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/profile" element={<Profile />} />
         <Route path="/" element={<Home />} />
         <Route path="/recherche" element={<Search />} />
-        <Route path="/profile/reclamations" component={<Reclamations />} />
-        <Route         <Route path="/details" element={<Produit />} />
+        <Route path="/profile/reclamations" element={<Reclamations />} />
+        <Route path="/details" element={<Produit />} />
         <Route path="/presentation" element={<Presentation />} />
-        <Route path="/profile/orders" component={Commandes} />
-path="*" element={<Error />} />
+        <Route path="/profile/orders" component={<Commandes />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
   </React.StrictMode>
