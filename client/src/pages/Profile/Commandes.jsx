b54f51db8b38c3914b67a43b721f339d33734aa1
Brainfeed-1996 <olivierrobertduboille@gmail.com>
2024-03-01 09:25:52 +0100

    ajout de la navbar

--- client/src/pages/Profile/Commandes.jsx
+++ client/src/pages/Profile/Commandes.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Commandes() {
-  return <div></div>;
+  return (
+    <>
+      <div className="Texte">
+        <h2>Mes commandes</h2>
+        <h3>Vous n'avez pas encore de commandes</h3>
+      </div>
+    </>
+  );
 }
--- client/src/pages/Profile/Reclamations.jsx
+++ client/src/pages/Profile/Reclamations.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import "./Profile.css";
+import { Navbar } from "../../components/Navbar/Navbar";
 
 export default function Reclamations() {
   return (
     <>
+      <Navbar />
       <div className="Texte">
         <h2>Mes réclamations</h2>
         <h3>Vous n'avez pas encore de réclamations</h3>
