e26020f26f68c8260b0615712c3669ec7a607811
Louis Le Cam <louislecam25@gmail.com>
2024-03-04 13:52:56 +0100

    Add remove equipment tests

--- server/tests/equipment/remove.test.ts
+++ server/tests/equipment/remove.test.ts
@@ -1,7 +1,83 @@
+import supertest from "supertest";
+
+import app from "../../src/app";
+import database from "../../src/database";
+import Equipment from "../../src/entities/Equipment";
+import { authentificate, initAndClearDatabase, insertAdmin, insertUser } from "../utils";
+
 describe('Remove equipment', () => {
-  test("todo", () => {
-    // TODO: Remove this test
-    // this is just any empty test because jest expect at list one test
-    expect(true).toBe(true);
+  beforeEach(async () => {
+    await initAndClearDatabase();
+
+    await insertAdmin();
+    await insertUser();
+
+    await database.getRepository(Equipment).insert({
+      name: "Some equipment",
+    });
   })
+
+  test("Remove equipment success", async () => {
+    const response = await supertest(app)
+      .delete("/equipments")
+      .auth(await authentificate("admin", "admin"), { type: "bearer" })
+      .send({ name: "Some equipment" });
+
+    const equipments = (await database.getRepository(Equipment).find()).map(equipment => equipment.name);
+
+    expect(response.status).toBe(200);
+    expect(response.text).toBe("OK");
+    expect(equipments).toStrictEqual([]);
+  });
+
+  test("Remove equipment inexistant", async () => {
+    const response = await supertest(app)
+      .delete("/equipments")
+      .auth(await authentificate("admin", "admin"), { type: "bearer" })
+      .send({ name: "Other equipment" });
+
+    const equipments = (await database.getRepository(Equipment).find()).map(equipment => equipment.name);
+
+    expect(response.status).toBe(404);
+    expect(response.text).toBe("Not Found");
+    expect(equipments).toStrictEqual(["Some equipment"]);
+  });
+
+  test("Remove equipment unauthorized connected as user", async () => {
+    const response = await supertest(app)
+      .delete("/equipments")
+      .auth(await authentificate("user", "user"), { type: "bearer" })
+      .send({ name: "Some equipment" });
+
+    const equipments = (await database.getRepository(Equipment).find()).map(equipment => equipment.name);
+
+    expect(response.status).toBe(401);
+    expect(response.text).toBe("Unauthorized");
+    expect(equipments).toStrictEqual(["Some equipment"]);
+  });
+
+  test("Remove equipment unauthorized not connected", async () => {
+    const response = await supertest(app)
+      .delete("/equipments")
+      .send({ name: "Some equipment" });
+
+    const equipments = (await database.getRepository(Equipment).find()).map(equipment => equipment.name);
+
+    expect(response.status).toBe(401);
+    expect(response.text).toBe("Unauthorized");
+    expect(equipments).toStrictEqual(["Some equipment"]);
+  });
+
+  test("Remove equipment unauthorized invalid token", async () => {
+    const response = await supertest(app)
+      .delete("/equipments")
+      .auth("adhzjfheuirhdeuhvserui:admin", { type: "bearer" })
+      .send({ name: "Some equipment" });
+
+    const equipments = (await database.getRepository(Equipment).find()).map(equipment => equipment.name);
+
+    expect(response.status).toBe(401);
+    expect(response.text).toBe("Unauthorized");
+    expect(equipments).toStrictEqual(["Some equipment"]);
+  });
 });
