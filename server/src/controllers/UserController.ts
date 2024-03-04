b5947d3168a2d3ddb5e89c285111136c2dadf2d3
Lehooperien1613 <doriandelpeux52@gmail.com>
2024-03-04 11:45:48 +0100

    add get user route

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { DeepPartial, QueryFailedError, getRepository  } from "typeorm";
+import { DeepPartial, QueryFailedError  } from "typeorm";
 import bcrypt from "bcrypt";
 
 import database from "../database";
@@ -118,74 +118,94 @@ export async function authentificate(req: Request, res: Response) {
   }
 }
 
-export async function deleteUser(req: Request, res: Response) {
-  let email: string = req.params.email;
-
+export async function remove(req: Request, res: Response) {
   try {
-    // Récupérer le référentiel d'utilisateur
-    const userRepository = database.getRepository(User);
-
-    // Rechercher l'utilisateur dans la base de données.
-    const user = await userRepository.findOne({ where: { email: email } });
-
-    // Vérifier si l'utilisateur existe.
-    if (!user) {
-      return res.status(404).json({ message: 'Utilisateur non trouvé' });
+    if (!await isSessionAdmin(req)) {
+      throw ControllerException.UNAUTHORIZED;
     }
 
-    // Supprimer l'utilisateur de la base de données.
-    await userRepository.remove(user);
+    let email: string = req.params.email;
 
-    return res.status(200).json({ message: 'Utilisateur supprimé avec succès' });
-  } catch (error) {
-    // Gérer les erreurs de suppression.
-    return res.status(500).json({ message: 'Erreur lors de la suppression de l\'utilisateur' });
+    const result = await database.getRepository(User).delete({ email: email });
+    if (result.affected == null || result.affected < 1) {
+      throw ControllerException.NOT_FOUND;
+    }
+
+    res.sendStatus(200);
+  } catch (err) {
+    return handle_controller_errors(res, err);
   }
 }
 
-export async function updateUser(req: Request, res: Response) {
-  const email = req.params.email;
-  const { first_name, last_name, password_hash, phone_number, admin }: DeepPartial<User> & { password: string }  = req.body;
-
+export async function modify(req: Request, res: Response) {
   try {
-    // Récupérer le référentiel d'utilisateur
-    const userRepository = database.getRepository(User);
+    if (!await isSessionAdmin(req)) {
+      throw ControllerException.UNAUTHORIZED;
+    }
+    
+    const email_params: string = req.params.email;
 
-    // Rechercher l'utilisateur dans la base de données.
-    let user = await userRepository.findOne({ where: { email: email } });
-
-    // Vérifier si l'utilisateur existe.
-    if (!user) {
-      return res.status(404).json({ message: 'Utilisateur non trouvé' });
+    const { 
+      first_name, 
+      last_name, 
+      email,
+      password, 
+      phone_number, 
+      admin,
+    } = req.body;
+  
+    if (
+      typeof first_name != "string" ||
+      typeof last_name != "string" ||
+      typeof email != "string" ||
+      typeof password != "string" ||
+      typeof phone_number != "string" ||
+      typeof admin != "boolean"
+      ) {
+      throw ControllerException.MALFORMED_REQUEST;
     }
 
-    // Mettre à jour les propriétés de l'utilisateur si elles sont fournies.
-    if (first_name) user.first_name = first_name;
-    if (last_name) user.last_name = last_name;
-    if (email) user.email = email;
-    if (password_hash) {
-      // Valider le nouveau mot de passe
-      if (!isValidPassword(password_hash)) {
-        return res.status(400).json({ message: 'Le mot de passe doit contenir au moins 8 caractères' });
+    const user = {     
+      first_name, 
+      last_name, 
+      email,
+      password_hash: await bcrypt.hash(password, 12), 
+      phone_number, 
+      admin,
+    };
+
+    try {
+      await database.getRepository(User).update({ email: email_params }, user);
+    } catch (err) {
+      if (err instanceof QueryFailedError && err.driverError.code == "23505") {
+        throw ControllerException.CONFLICT;
       }
-      // Hacher et enregistrer le nouveau mot de passe
-      user.password_hash = await bcrypt.hash(password_hash, 12);
+      throw err;
     }
-    if (phone_number) user.phone_number = phone_number;
-    if (admin !== undefined) user.admin = admin;
 
-    // Enregistrer les modifications dans la base de données.
-    await userRepository.save(user);
-
-    return res.status(200).json({ message: 'Utilisateur mis à jour avec succès' });
-  } catch (error) {
-    // Gérer les erreurs de mise à jour.
-    return res.status(500).json({ message: 'Erreur lors de la mise à jour de l\'utilisateur' });
+    res.sendStatus(200);
+  } catch (err) {
+    return handle_controller_errors(res, err);
   }
 }
 
-// Fonction de validation du mot de passe
-function isValidPassword(password_hash: string): boolean {
-  // Ajoutez vos règles de validation du mot de passe ici
-  return password_hash.length >= 12;
+export async function get(req: Request, res: Response) {
+  try {
+    if (!await isSessionAdmin(req)) {
+      throw ControllerException.UNAUTHORIZED;
+    }
+    
+    const email_params: string = req.params.email;
+  
+    const result = await database.getRepository(User).findOne({
+      select: ["first_name", "last_name", "email", "phone_number", "admin"],
+      where: {
+        email: email_params
+      }
+    });
+
+    res.status(200).send(result);
+  } catch (err) {
+    handle_controller_errors(res, err);
+  }
 }
