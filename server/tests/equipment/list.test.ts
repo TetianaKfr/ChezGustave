59b0d047ebdb6cab5effcfc3a0b277717dc021c7
Louis Le Cam <louislecam25@gmail.com>
2024-03-04 13:29:01 +0100

    Add list equipments tests

--- server/tests/equipment/list.test.ts
+++ server/tests/equipment/list.test.ts
@@ -1,7 +1,61 @@
+import supertest from "supertest";
+import { authentificate, initAndClearDatabase, insertAdmin, insertUser } from "../utils";
+import app from "../../src/app";
+import database from "../../src/database";
+import Equipment from "../../src/entities/Equipment";
+
 describe('List equipements', () => {
-  test("todo", () => {
-    // TODO: Remove this test
-    // this is just any empty test because jest expect at list one test
-    expect(true).toBe(true);
-  })
+  beforeEach(async () => {
+    await initAndClearDatabase();
+
+    await insertUser();
+    await insertAdmin();
+
+    await database.getRepository(Equipment).insert([
+      { name: "Equipment 1" },
+      { name: "Equipment 2" },
+      { name: "Equipment 3" },
+    ]);
+  });
+
+  test("List equipments success connected as admin", async () => {
+    const response = await supertest(app)
+      .get("/equipments")
+      .auth(await authentificate("admin", "admin"), { type: "bearer" });
+
+    expect(response.status).toBe(200);
+    expect(response.body).toContain("Equipment 1");
+    expect(response.body).toContain("Equipment 2");
+    expect(response.body).toContain("Equipment 3");
+    expect(response.body.length).toBe(3);
+  });
+
+  test("List equipments success connected as user", async () => {
+    const response = await supertest(app)
+      .get("/equipments")
+      .auth(await authentificate("user", "user"), { type: "bearer" });
+
+    expect(response.status).toBe(200);
+    expect(response.body).toContain("Equipment 1");
+    expect(response.body).toContain("Equipment 2");
+    expect(response.body).toContain("Equipment 3");
+    expect(response.body.length).toBe(3);
+  });
+
+  test("List equipments unauthorized not connected", async () => {
+    const response = await supertest(app)
+      .get("/equipments");
+
+    expect(response.status).toBe(401);
+    expect(response.text).toBe("Unauthorized");
+  });
+
+  test("List equipments unauthorized invalid token", async () => {
+    const response = await supertest(app)
+      .get("/equipments")
+      .auth("dqdqzjdzhfjklrhquidqh:admin", { type: "bearer" });
+
+    expect(response.status).toBe(401);
+    expect(response.text).toBe("Unauthorized");
+  });
 });
