0a4c426bf81b115688157ada49bf7a6b9e9629be
Louis Le Cam <louislecam25@gmail.com>
2024-03-04 16:18:05 +0100

    Renamed equipment routes

--- server/src/routes/EquipmentRoutes.ts
+++ server/src/routes/EquipmentRoutes.ts
@@ -5,9 +5,9 @@ import * as EquipmentController from "../controllers/EquipmentController";
 const router = Router();
 
 router.get('/equipments', EquipmentController.list);
-router.post('/equipments', EquipmentController.create);
-router.put('/equipments', EquipmentController.modify);
-router.delete('/equipments', EquipmentController.remove);
+router.post('/equipment', EquipmentController.create);
+router.put('/equipment', EquipmentController.modify);
+router.delete('/equipment', EquipmentController.remove);
 
 export default router;
 
