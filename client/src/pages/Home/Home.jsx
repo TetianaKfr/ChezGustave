594659c904fa02ff0a08f902d05bdd777a964eab
Carl Richard <carl.richard@institutsolacroup.com>
2024-03-04 09:15:40 +0100

    Ajout Page home Css

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,13 +2,61 @@ import React from 'react';
 import { Navbar } from '../../components/Navbar/Navbar';
 import { Searchbar } from '../../components/SearchBar/Searchbar';
 import { Footer } from '../../components/Footer/Footer';
+import './Home.css'
+import Banner from '../../assets/PresentationLake.jpg';
 
 export const Home = () => {
     return (
         <>
-            <Navbar/>
-            <Searchbar/>
-            <Footer/>
+            <Navbar />
+            <img className='heroBanner' src={Banner} alt="" />
+            <Searchbar />
+            <div className='wrapper'>
+
+                <div className='backgColor'>
+                    <h2>Voyager en France</h2>
+                    <p>Pourquoi pas ici?</p>
+                    <div className='displayVoyage'>
+                        <div>
+                            <img className='photoDestination' src="" alt="" />
+                            <p>Lieu</p>
+                        </div>
+                        <div>
+                            <img className='photoDestination' src="" alt="" />
+                            <p>Lieu</p>
+                        </div>
+                        <div>
+                            <img className='photoDestination' src="" alt="" />
+                            <p>Lieu</p>
+                        </div>
+                        <div>
+                            <img className='photoDestination' src="" alt="" />
+                            <p>Lieu</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='coupCoeur'>
+                    <h2 className='colorText'>Laissez-vous tenter par nos coups de coeur</h2>
+                    <p className='colorText'>Nos locations les mieux notées</p>
+
+                    <div className='displayCoeur'>
+                        <div className='cardCoeur'>
+                            <img className='vignetteCoeur' src="" alt="" />
+                            <p>Voir le descriptif</p>
+                        </div>
+                        <div className='cardCoeur'>
+                            <img className='vignetteCoeur' src="" alt="" />
+                            <p>Voir le descriptif</p>
+                        </div>
+                        <div className='cardCoeur'>
+                            <img className='vignetteCoeur' src="" alt="" />
+                            <p>Voir le descriptif</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer />
         </>
     )
 }
