604281e476c1d874e5e57358b81aa3de7c7f9574
Carl Richard <carl.richard@institutsolacroup.com>
2024-03-05 15:57:28 +0100

    bug responsive + ajout background

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import { Searchbar } from '../../components/SearchBar/Searchbar';
 import { Footer } from '../../components/Footer/Footer';
 import './Home.css'
 import Banner from '../../assets/heroBanner.png';
+import Background from '../../assets/BackgroundDestination.jpg'
 
 export const Home = () => {
     return (
@@ -12,25 +13,25 @@ export const Home = () => {
             <img className='heroBanner' src={Banner} alt="Image d'accueil du site" />
             <Searchbar />
             <div className='wrapper displayHome'>
-                <div className='backgColor'>
-                    <h2>Voyager en France</h2>
-                    <p>Pourquoi pas ici?</p>
-                    <div className='displayVoyage'>
+                <div className='backgColor' style={{ backgroundImage: `url(${Background})` }}>
+                    <h2 className='Taille'>Voyager en France</h2>
+                    <p className='Taille2'>Pourquoi pas ici?</p>
+                    <div className='displayVoyage' >
                         <div>
-                            <img className='photoDestination' src="" alt="" />
-                            <p>Lieu</p>
+                            <img className='photoDestination' src={Banner}  alt="" />
+                            <p className='backgroundColor'>Le Havre</p>
                         </div>
                         <div>
-                            <img className='photoDestination' src="" alt="" />
-                            <p>Lieu</p>
+                            <img className='photoDestination' src={Banner}  alt="" />
+                            <p className='backgroundColor'>Lyon</p>
                         </div>
                         <div>
-                            <img className='photoDestination' src="" alt="" />
-                            <p>Lieu</p>
+                            <img className='photoDestination' src={Banner} alt="" />
+                            <p className='backgroundColor'>Montpellier</p>
                         </div>
                         <div>
-                            <img className='photoDestination' src="" alt="" />
-                            <p>Lieu</p>
+                            <img className='photoDestination' src={Banner} alt="" />
+                            <p className='backgroundColor'>Strasbourg</p>
                         </div>
                     </div>
                 </div>
