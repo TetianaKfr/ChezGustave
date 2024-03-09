d4ff4eefd2548031a8d05dc85788d8768c7abca1
Carl Richard <carl.richard@institutsolacroup.com>
2024-03-09 10:50:15 +0100

    modif barre de recherche

--- client/src/components/SearchBar/Searchbar.jsx
+++ client/src/components/SearchBar/Searchbar.jsx
@@ -7,7 +7,7 @@ export const Searchbar = () => {
         const currentDate = new Date();
         const day = currentDate.getDate();
         const month = currentDate.getMonth() + 1; // demmare a 0 par defaut
-        const year = currentDate.getYear() 
+        const year = currentDate.getYear()
 
         // Ajouter un 0 devant le jour et le mois s'ils sont inférieurs à 10
         const formattedDay = day < 10 ? `0${day}` : day;
@@ -19,38 +19,36 @@ export const Searchbar = () => {
     return (
         <>
             <div className='searchbar'>
-                <div className='destination'>
-                    <label htmlFor="">Ou allez vous ?</label>
-                    <input placeholder='ville'   type="text" />
+                <div className='column'>
+                <label>Destination</label>
+                <select id="environment">
+                    <option value="Montagne">Montagne</option>
+                    <option value="Campagne">Campagne</option>
+                    <option value="BordMer">Bord de Mer</option>
+                </select>
                 </div>
 
                 <div className='row'>
-                    <div className='horaires'>
+                    <div className='column'>
                         <label htmlFor="">Départ</label>
-                        <input  type="date" defaultValue={afficheDate()} />
+                        <input type="date" defaultValue={afficheDate()} />
                     </div>
-                    <div className='horaires'>
+                    <div className='column'>
                         <label htmlFor="">Arrivée</label>
                         <input type="date" defaultValue={afficheDate()} />
                     </div>
                 </div>
 
-                <div className='row'>
-                    <div className='nombrePersonnes'>
-                        <label htmlFor="">Adultes</label>
+                <div className='nombreChambre'>
+                    <div className='column'>
+                        <label htmlFor="">Chambre</label>
                         <input placeholder='0' className='InputNb' type="text" />
                     </div>
-                    <div className='nombrePersonnes'>
-                        <label htmlFor="">Enfants</label>
-                        <input placeholder='0' className='InputNb' type="text" />
-                    </div>
-                </div>                    
-                <div className='nombrePersonnes'>
-                    <label htmlFor="">Chambre</label>
-                    <input placeholder='0' className='InputNb' type="text" />
+
                 </div>
+
                 <NavLink to="/recherche">
-                    <button id='btnSearch'>Rechercher</button>
+                    <button id='btnSearch'>Affiner ma recherche</button>
                 </NavLink>
             </div>
         </>
