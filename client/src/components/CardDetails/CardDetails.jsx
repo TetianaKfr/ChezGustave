50ccdc24c84ee5685d6be83f37615716883451b3
Carl Richard <carl.richard@institutsolacroup.com>
2024-03-12 11:03:49 +0100

    ajout fetch et afficheage des cartes logements dans la recherche

--- client/src/components/CardDetails/CardDetails.jsx
+++ client/src/components/CardDetails/CardDetails.jsx
@@ -7,42 +7,22 @@ export const CardDetails = ({housing}) => {
     <>
       <div className="cardComplete">
         <div className="CardDetails">
-          <img className="photoLogement" src="" alt="photo du logement" />
+          <img className="photoLogement" src={housing.images_urls[0]} alt="photo du logement" />
         </div>
         <span className="UseForCSS"></span>
         <div className="textCard">
           <div className="row gap">
-            <h2>{housing}</h2>
+            <h2>{housing.area}</h2>
             <div className="divPicto">
               <img src={pictoVille} alt="pictogramme ville" />
-              <p>catégories</p>
+              <p>{housing.category}</p>
             </div>
           </div>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-            ipsum ut deleniti. Ducimus, eos! Saepe laboriosam culpa recusandae
-            repellat quasi illum deleniti. Iste nostrum mollitia assumenda
-            beatae maioreipsum dolor sit amet consectetur adipisicing elit.
-            Voluptatem ipsum ut deleniti. Ducimus, eos! Saepe laboriosam culpa
-            recusandae repellat quasi illum deleniti. Iste nostrum mollitia
-            assumenda beatae maiores temporibus voluptatum.Lorem ipsum dolor sit
-            amet consectetur adipisicing elit. Voluptatem ipsum ut deleniti.
-            Ducimus, eos! Saepe laboriosam culpa recusandae repellat quasi illum
-            deleniti. Iste nostrum mollitia assumenda beatae maiores temporibus
-            voluptatum. Lorem ipsum dolor, sit amet consectetur adipisicing
-            elit. Sunt alias fugiat nam facilis tempora, omnis neque debitis
-            voluptate aut quidem distinctio quia exercitationem dolore quas
-            minima error, dolorum nostrum doloremque hic nihil numquam tenetur
-            at! Praesentium vero tempore neque deleniti excepturi dicta, nemo
-            cupiditate atque, sit quidem explicabo libero, maxime expedita
-            consequuntur id laudantium vel. Beatae, voluptatem excepturi, sed
-            vero consectetur aspernatur rem fugiat quidem quae commodi placeat
-            animi quam molestiae saepe sunt minima doloremque, totam culpa
-            perspiciatis pariatur optio! Maxime dignissimos provident eum,
-            ratione neque officia ut sunt est libero esse, dolor laboriosam
-            minus, sed dicta perspiciatis deleniti ipsa?
+            {housing.description}
           </p>
-          <p className="row">Equipements : </p>
+          <p>prix: {housing.medium_price}</p>
+          <p className="row"> Chambres: {housing.bedroom_count}  / Salle de bain: {housing.bathroom_count}  </p>
         </div>
       </div>
     </>
