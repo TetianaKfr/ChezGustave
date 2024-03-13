35915162ff7096f0e9c66caa4fa062ecb8c78bb4
Carl Richard <carl.richard@institutsolacroup.com>
2024-03-13 11:28:40 +0100

    ajout img home, changement connexion

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,19 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import "../component.css";
 import LogoGustave from "../../assets/LogoGustave.png";
 import { NavLink } from "react-router-dom";
+import LoginContext, { SessionState } from "../../LoginContext";
 
 export const Navbar = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState('');
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const { isLoggedIn, setIsLoggedIn } = useContext(LoginContext);
 
-  useEffect(() => {
-      // On récupère le token complet depuis le localStorage.
+  useEffect( () => {
+    // On récupère le token complet depuis le localStorage.
     const full_token = localStorage.getItem("token");
-      // si le token existe.
+    // si le token existe.
     if (full_token) {
       //decoupe du token pour recup l'adresse mail , on recupere la partie apres : 
       const [_token, ...email_parts] = full_token.split(':');
@@ -21,8 +22,30 @@ export const Navbar = () => {
       const email = email_parts.join(':');
       // on met a jour le setter
       setEmail(email);
-      //indication que l'utilisateur est log
-      setIsLoggedIn(true);
+      (async () => {
+        try {
+          const response = await fetch("http://localhost:3630/user", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": "Bearer " + localStorage.getItem("token"),
+            },
+            body: JSON.stringify({ email }),
+          });
+
+          if (response.ok) {
+            if ((await response.json()).admin) {
+              setIsLoggedIn(SessionState.Admin);
+            } else {
+              setIsLoggedIn(SessionState.Connected);
+            }
+          } else {
+            console.error("erreur reponse", response);
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      })();
     }
   }, []);
   // Les crochets vides [] signifient que cette fonction ne dépend d'aucune variable et ne doit être exécutée qu'une seule fois.
@@ -32,7 +55,7 @@ export const Navbar = () => {
   };
 
   const handleLogout = () => {
-    setIsLoggedIn(false);
+    setIsLoggedIn(SessionState.NotConnected);
     localStorage.removeItem("token");
   };
 
@@ -58,66 +81,87 @@ export const Navbar = () => {
         const { token } = await response.json();
         localStorage.setItem("token", token);
         handleModalClose();
-        setIsLoggedIn(true);
+        try {
+          const response = await fetch("http://localhost:3630/user", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": "Bearer " + localStorage.getItem("token"),
+            },
+            body: JSON.stringify({ email }),
+          });
+
+          if (response.ok) {
+            if ((await response.json()).admin) {
+              setIsLoggedIn(SessionState.Admin);
+            } else {
+              setIsLoggedIn(SessionState.Connected);
+            }
+          } else {
+            console.error("erreur reponse", response);
+          }
+        } catch (error) {
+          console.error(error);
+        };
       } else if (response.status === 401) {
-        setErrorMessage('Mail ou mot de passe incorrect')
+        setErrorMessage('Mail ou mot de passe incorrect');
       } else {
-        setErrorMessage('Une erreur est survenue lors de la connexion')
+        setErrorMessage('Une erreur est survenue lors de la connexion');
       }
     } catch (error) {
-      setErrorMessage('Erreur de connexion')
+      setErrorMessage('Erreur de connexion');
     }
   };
 
 
-return (
-  <>
-    <div className="navbar">
-      <NavLink to="/">
-        <img id="Logo" src={LogoGustave} alt="logo site" />
-      </NavLink>
+  return (
+    <>
+      <div className="navbar">
+        <NavLink to="/">
+          <img id="Logo" src={LogoGustave} alt="logo site" />
+        </NavLink>
 
-      <div className="Boutons">
-        {isLoggedIn ? (
-          <>
-            <p className="welcomeMsg">Bienvenue {email}</p>
-            <button onClick={handleLogout}>Déconnexion</button>
-          </>
-        ) : (
-          <>
-            <button onClick={handleLogin}>Se connecter</button>
-          </>
-        )}
-      </div>
-    </div>
-
-    {isModalOpen && (
-      <div className="modal">
-        <div className="modal-content">
-          <span className="close" onClick={handleModalClose}></span>
-          <form onSubmit={handleFormSubmit}>
-            <label className="" htmlFor="email">Identifiant :</label>
-            <input
-              type="text"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <label htmlFor="password">Mot de passe :</label>
-            <input
-              type="password"
-              placeholder="********"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <button type="submit">Valider</button>
-          </form>
-          {errorMessage && (
-            <div style={{ color: 'red', marginTop: '10px' }}>{errorMessage}</div>
+        <div className="Boutons">
+          {isLoggedIn!=SessionState.NotConnected ? (
+            <>
+              <p className="welcomeMsg">Bienvenue {email}</p>
+              <button onClick={handleLogout}>Déconnexion</button>
+            </>
+          ) : (
+            <>
+              <button onClick={handleLogin}>Se connecter</button>
+            </>
           )}
         </div>
       </div>
-    )}
-  </>
-);
+
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={handleModalClose}></span>
+            <form onSubmit={handleFormSubmit}>
+              <label className="" htmlFor="email">Identifiant :</label>
+              <input
+                type="text"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <label htmlFor="password">Mot de passe :</label>
+              <input
+                type="password"
+                placeholder="********"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button type="submit">Valider</button>
+            </form>
+            {errorMessage && (
+              <div style={{ color: 'red', marginTop: '10px' }}>{errorMessage}</div>
+            )}
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
